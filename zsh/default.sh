8e268ec18ad76b44aa16ec9f0ff7c17b490a00dc
Ryohei Fushimi <rfushimi@users.noreply.github.com>
2022-11-06 22:54:36 +0900

    Update by 'update_dotfiles'

--- zsh/default.sh
+++ zsh/default.sh
@@ -34,5 +34,21 @@ bash*)
     PROMPT_COMMAND='share_history'
     shopt -u histappend
     export HISTSIZE=2000
+
+    alias -s {md,markdown,txt}='vim'
+    shopt -s autocd
+
+    _suffix_vim=(md markdown txt text tex cc c C cxx h hh java py rb sh)
+    command_not_found_handle () {
+    if [ -f "$1" ];then
+        if echo " ${_suffix_vim[*]} "|grep -q "${1##*.}";then
+        echo "$cmd is a file, open $cmd with vi..."
+        vi "$1"
+        return $?
+        fi
+    fi
+    echo "bash: $1: command not found"
+    return 127
+    }
     ;;
 esac
---
+++ zsh/hosts/nucbox3.sh
@@ -0,0 +1,1 @@
+echo -ne '\033]1337;SetColors=bg=200\a' # to red
