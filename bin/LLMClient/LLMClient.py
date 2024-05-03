509d1839859cbc2869ed4fdbb3a8182ec9d5aaba
Ryohei Fushimi <rfushimi@users.noreply.github.com>
2024-05-03 15:57:29 +0900

    Update by 'update_dotfiles'

---
+++ bin/LLMClient/LLMClient.py
@@ -0,0 +1,31 @@
+import anthropic
+import sys
+
+class LLMClient:
+  SYSTEM_PROMPT_SELF = "User is senior software engineer whose ESL speaker working for Google."
+  SYSTEM_PROMPT_INST = "Make the following sentences in English natural and idiomatic, or translate if necessary. "
+  SYSTEM_PROMPT_EXTRA = "It will be used for internal technical discussion, so it’s expected to be more concise, to-the-point, instead of being polite or lengthy."
+
+  def __init__(self):
+    # Read API key from file
+    with open('/Users/ryohei/keys/claude.key', 'r') as file:
+      api_key = file.read().rstrip('\n')
+    self.api_key = api_key
+    self.client = anthropic.Anthropic(api_key=self.api_key)
+  
+  def generate(self, system, input):
+    print(input)
+    message = self.client.messages.create(
+      model="claude-3-sonnet-20240229",
+      max_tokens=1000,
+      system=system,
+      messages=[
+        {
+          "role": "user",
+          "content": input
+        }
+      ]
+    )
+    print(message.content)
+    return message.content[0].text
+  
---
+++ bin/claude.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import anthropic
+import sys
+import LLMClient.LLMClient as LLMClient
+
+# Create LLM client instance
+llm_client = LLMClient.LLMClient()
+data = sys.stdin.read()
+
+print(data)
+print("\n")
+
+system_prompt = llm_client.SYSTEM_PROMPT_SELF + llm_client.SYSTEM_PROMPT_INST + llm_client.SYSTEM_PROMPT_EXTRA
+result = llm_client.generate(system = system_prompt, input = data)
+
+# Print the generated sonnet
+print(result)
