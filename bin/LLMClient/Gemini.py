5598d3ae08e575eabf82025fffce2a3078ef14b2
Ryohei Fushimi <rfushimi@users.noreply.github.com>
2024-05-15 16:52:16 +0900

    Update by 'update_dotfiles'

--- bin/LLMClient/Gemini.py
+++ bin/LLMClient/Gemini.py
@@ -4,8 +4,10 @@ import google.generativeai as genai
 
 class LLMClientGemini:
   SYSTEM_PROMPT_SELF = "User is senior software engineer whose ESL speaker working for Google."
-  SYSTEM_PROMPT_INST = "Make the following sentences in English natural and idiomatic, or translate if necessary. "
+  SYSTEM_PROMPT_INST_WRITING = "Make the following sentences in English natural and idiomatic, or translate if necessary. "
   SYSTEM_PROMPT_EXTRA = "It will be used for internal technical discussion, so it’s expected to be more concise, to-the-point, instead of being polite or lengthy."
+
+  SYSTEM_PROMPT_INST_READING = "Translate the following paragraphs to Japanese. You may leave technical proper nouns in English (such as names of language or library) but translate general terms (e.g. compiler to コンパイラ). "
 
   def __init__(self):
     # Read API key from file
@@ -14,7 +16,12 @@ class LLMClientGemini:
     genai.configure(api_key=api_key)
     self.client = genai.GenerativeModel('gemini-pro')
   
-  def generate(self, system, input):
+  def translate(self, system, input):
+    response = self.client.generate_content(system + "\n\n" + input)
+    response.resolve()
+    return response.text
+
+  def translateEnToJa(self, system, input):
     response = self.client.generate_content(system + "\n\n" + input)
     response.resolve()
     return response.text
--- bin/translate.py
+++ bin/translate.py
@@ -3,14 +3,24 @@
 import sys
 # import LLMClient.Claude as Claude
 from LLMClient.Gemini import LLMClientGemini
+import argparse
 
-# Create LLM client instance
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--task", help="Specify the task")
+args = parser.parse_args()
+
+# Rest of the code
 llm_client = LLMClientGemini()
 data = sys.stdin.read()
 
-system_prompt = llm_client.SYSTEM_PROMPT_SELF + llm_client.SYSTEM_PROMPT_INST + llm_client.SYSTEM_PROMPT_EXTRA
-result = llm_client.generate(system = system_prompt, input = data)
+system_prompt = ""
+# Determine system prompt based on task
+if args.task == "writing":
+  system_prompt = system_prompt = llm_client.SYSTEM_PROMPT_SELF + llm_client.SYSTEM_PROMPT_INST_WRITING + llm_client.SYSTEM_PROMPT_EXTRA
+elif args.task == "reading":
+  system_prompt = system_prompt = llm_client.SYSTEM_PROMPT_SELF + llm_client.SYSTEM_PROMPT_INST_READING + llm_client.SYSTEM_PROMPT_EXTRA
 
-# Print the generated sonnet
+result = llm_client.translate(system=system_prompt, input=data)
 print(data)
 print(result)
