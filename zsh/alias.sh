bc70106732bc53313f2a7219bc9870ddc496cfde
Ryohei Fushimi <rfushimi@users.noreply.github.com>
2024-08-05 12:23:28 +0900

    Update by 'update_dotfiles'

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -6,10 +6,9 @@ alias ssh-keygen="ssh-keygen -t ed25519 -a 128"
 
 alias dotfiles="code ~/dotfiles"
 alias ud="update_dotfiles"
+alias update_dotfiles="update_dotfiles"
 
 # save current directory and source ~/.zshrc and get back to the directory
 alias reload="pwd > ~/.zshrc_pwd; source ~/.zshrc; cd \`cat ~/.zshrc_pwd\`"
 
 alias w="npx wrangler"
-
-alias update_dotfiles = update_dotfiles
