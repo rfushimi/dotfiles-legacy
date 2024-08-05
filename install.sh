7d6158300e87aa07437bf6b766e37ca0d1ab735b
Ryohei Fushimi <rfushimi@users.noreply.github.com>
2024-08-05 10:00:27 +0900

    Update by 'update_dotfiles'

--- install.sh
+++ install.sh
@@ -77,7 +77,3 @@ darwin*)
     
     ;;
 esac
-
-wget "https://github.com/GitCredentialManager/git-credential-manager/releases/download/v2.0.696/gcmcore-linux_amd64.2.0.696.deb" -O /tmp/gcmcore.deb
-sudo dpkg -i /tmp/gcmcore.deb
-git-credential-manager-core configure
