0b7f18bb602e76f5da95228201082356e79f2e62
Ryohei Fushimi <rfushimi@users.noreply.github.com>
2024-12-10 17:16:28 +0900

    Update by 'update_dotfiles'

--- zsh/path.sh
+++ zsh/path.sh
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-
-PATH="$HOME/dotfiles/bin:$PATH"
-PATH="$HOME/bin:$PATH"
-PATH="/opt/qemu/bin:$PATH"
-PATH="/opt/homebrew/bin:$PATH"
-PATH="$HOME/.rye/shims:$PATH"
-PATH="/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH"
-=======
 export PATH="$HOME/dotfiles/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
 export PATH="/opt/qemu/bin:$PATH"
 export PATH="/opt/homebrew/bin:$PATH"
 export PATH="/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH"
 export PATH=/Users/fushimi/Library/Python/3.13/bin:$PATH
->>>>>>> 2bce0901cb (Update by 'update_dotfiles')
