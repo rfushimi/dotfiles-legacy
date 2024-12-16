a146e7522fef85d4bf93397a716a53692169d502
Ryohei Fushimi <rfushimi@users.noreply.github.com>
2024-12-16 13:11:56 +0900

    Update by 'update_dotfiles'

--- zsh/arch/arm64.sh
+++ zsh/arch/arm64.sh
@@ -1,3 +1,6 @@
 echo "Archtecture: arm64"
 
 HOMEBREW_DIR="/opt/homebrew"
+
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_AUTO_UPDATE_SECS=864000
--- zsh/path.sh
+++ zsh/path.sh
@@ -3,4 +3,5 @@ export PATH="$HOME/bin:$PATH"
 export PATH="/opt/qemu/bin:$PATH"
 export PATH="/opt/homebrew/bin:$PATH"
 export PATH="/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH"
-export PATH=/Users/fushimi/Library/Python/3.13/bin:$PATH
+# export PATH=/Users/fushimi/Library/Python/3.13/bin:$PATH
+export PATH="$HOME/.pdm/global/.venv/bin:$PATH"
